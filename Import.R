1cbe0b56a6f6a539034090dfe84778c90abd4d37
Péter Kalicz <kaliczp@gmail.com>
2026-08-26 16:54:01 +0200

    Separate with split() function
    A split() függvénnyel egy csatornánkénti listába teszem.

--- Import.R
+++ Import.R
@@ -3,3 +3,6 @@ dir("Met_allomas_Fertodkontrol/202606/")
 teszt <- read.csv("Met_allomas_Fertodkontrol/202606/25.CSV", head = FALSE)
 ## Felfedezés
 names(teszt)
+str(teszt)
+csatorna_lista <- split(teszt, teszt[[3]])
+str(csatorna_lista)
