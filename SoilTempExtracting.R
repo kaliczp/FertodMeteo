1c468b0df64ff76b996f0fde74b609f271bcb94b
Péter Kalicz <kaliczp@gmail.com>
2026-09-03 15:13:00 +0200

    Reindent function
    Nincs változás, csak újra formáztam.

--- SoilTempExtracting.R
+++ SoilTempExtracting.R
@@ -13,47 +13,48 @@ C14_1 <- C14[sensor_nr == 1]
 C14_2 <- C14[sensor_nr == 2]
 C14_3 <- C14[sensor_nr == 3]
 
-## Split sensors
+### Split sensors
 split_sensors <- function(sensor) {
 
-  # Az időbélyegek
-  tt <- index(sensor)
+    ## Az időbélyegek
+    tt <- index(sensor)
 
-  # Hány mérés tartozik az egyes időpontokhoz?
-  n <- ave(seq_along(tt), tt, FUN = length)
+    ## Hány mérés tartozik az egyes időpontokhoz?
+    n <- ave(seq_along(tt), tt, FUN = length)
 
-  # Az adott időbélyegen belüli sorszám
-  pos <- ave(seq_along(tt), tt, FUN = seq_along)
+    ## Az adott időbélyegen belüli sorszám
+    pos <- ave(seq_along(tt), tt, FUN = seq_along)
 
-  # Problémás időpontok:
-  # nem 3 és nem 4 mérés tartozik hozzá
-  bad_times <- unique(tt[n != 3 & n != 4])
+    ## Problémás időpontok:
+    ## nem 3 és nem 4 mérés tartozik hozzá
 
-  # A 4 mérésből álló csoportokban a 2. mérés
-  # a duplikált első csatorna -> ezt eldobjuk
-  keep <- !(n == 4 & pos == 2)
+    bad_times <- unique(tt[n != 3 & n != 4])
 
-  sensor_clean <- sensor[keep]
+    ## A 4 mérésből álló csoportokban a 2. mérés
+    ## a duplikált első csatorna -> ezt eldobjuk
+    keep <- !(n == 4 & pos == 2)
 
-  # Újra meghatározzuk a pozíciót a megtisztított adatokon
-  tt_clean <- index(sensor_clean)
+    sensor_clean <- sensor[keep]
 
-  pos_clean <- ave(
-    seq_along(tt_clean),
-    tt_clean,
-    FUN = seq_along
-  )
+    ## Újra meghatározzuk a pozíciót a megtisztított adatokon
+    tt_clean <- index(sensor_clean)
 
-  # A három szenzor
-  sensor1 <- sensor_clean[pos_clean == 1]
-  sensor2 <- sensor_clean[pos_clean == 2]
-  sensor3 <- sensor_clean[pos_clean == 3]
+    pos_clean <- ave(
+        seq_along(tt_clean),
+        tt_clean,
+        FUN = seq_along
+    )
 
-  # Eredmény visszaadása
-  list(
-    sensor1 = sensor1,
-    sensor2 = sensor2,
-    sensor3 = sensor3,
-    bad_times = bad_times
-  )
+    ## A három szenzor
+    sensor1 <- sensor_clean[pos_clean == 1]
+    sensor2 <- sensor_clean[pos_clean == 2]
+    sensor3 <- sensor_clean[pos_clean == 3]
+
+    ## Eredmény visszaadása
+    list(
+        sensor1 = sensor1,
+        sensor2 = sensor2,
+        sensor3 = sensor3,
+        bad_times = bad_times
+    )
 }
